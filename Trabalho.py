827e3f5d04af85f9b93b8205b5709590f4bad94d
Apollo-Hb <apollohb0@gmail.com>
2024-06-16 18:56:59 -0300

    Commit

---
+++ Trabalho.py
@@ -0,0 +1,188 @@
+"""
+tera uma lista de alunos, com as notas de b1 e b2,
+um menu para selecionar a opção
+1 - adicionar aluno
+2 - listar aluno
+3 - remover aluno
+4 - procurar aluno
+5 - aprovados
+6 - reprovados
+7 - procurar pelo nome do aluno
+8 - media b1
+9 - media b2
+10 - media geral da turma
+11 - Diário
+0 - sair
+"""
+
+alunos = {}
+
+def menu():
+    print("1 - Adicionar aluno")
+    print("2 - listar aluno")
+    print("3 - Remover aluno")
+    print("4 - procurar aluno")
+    print("5 - Aprovados")
+    print("6 - Reprovados")
+    print("7 - procurar pelo nome do aluno")
+    print("8 - Média da turma B1")
+    print("9 - Média da turma B2")
+    print("10 -Média da turma GERAL" )
+    print("11 - Diário")
+    print(" 0 - sair")
+    try:
+        opcao = int(input("Escolha uma opção: "))
+        return opcao
+    except Exception as e:
+        print(f"Opção invalida: {e}")
+def add_aluno():
+    try:
+        ra = input("digite a ra do aluno: ")
+        nome =  input("digite o nome do aluno: ")
+        nota_b1 = float(input("digite a nota b1 do aluno: "))
+        nota_b2 = float(input("digite a nota b2 do aluno: "))
+        dados = {"nome": nome, "b1": nota_b1, "b2": nota_b2}
+        alunos[ra] = dados
+    except Exception as e:
+        print(f" alguma coisa foi digitada errado!:")
+def listar_aluno():
+    for ra, dados in alunos.items():
+        print(f"RA: {ra} - aluno: {dados['nome']} - B1: {dados['b1']} - B2: {dados['b2']}")
+    input("presione qualquer tecla para continuar!")
+    
+def remover_aluno():
+    ra = input("Digite a Ra do aluno")
+    if ra in alunos:
+        aluno = alunos.pop(ra)
+        print(f"O aluno: {aluno["nome"]} foi removido")
+    else:
+        print("aluno não encontrado!")
+    input("presione qualquer tecla para continuar!")
+    
+def procurar_aluno():
+    ra = input("Digite a Ra do aluno: ")
+    if ra in alunos:
+        dados = alunos[ra]
+        print(f"RA: {ra} - nome: {dados["nome"]}")
+    else:
+        print("Aluno não encontrado!")
+    input("presione qualquer tecla para continuar!")
+    
+def aprovados():
+    for ra, dados in alunos.items():
+        if ((dados["b1"] + dados["b2"]) / 2) >= 7.0:
+            aluno = f"RA: {ra} - "
+            aluno += f"Nome: {dados["nome"]} - "
+            aluno += f"B1: {dados["b1"]} - "
+            aluno += f"B2: {dados["b2"]} - "
+            print(aluno)
+    input("presione qualquer tecla para continuar!")
+
+def reprovados():
+    for ra, dados in alunos.items():
+        if ((dados["b1"] + dados["b2"]) / 2) < 7.0:
+            aluno = f"RA: {ra} - "
+            aluno += f"Nome: {dados["nome"]} - "
+            aluno += f"B1: {dados["b1"]} - "
+            aluno += f"B2: {dados["b2"]} - "
+            print(aluno)
+    input("presione qualquer tecla para continuar!")
+    
+def procurar_nome():
+    nome = input("Digite o nome do aluno: ")
+    for ra, dados in alunos.items():
+        if dados['nome'] == nome:
+            print(f"RA: {ra} - Nome: {dados['nome']} - B1: {dados['b1']} - B2: {dados['b2']}")
+    input("presione qualquer tecla para continuar!")
+
+def media_b1(alunos):
+    if alunos:
+        soma = 0
+        contador = 0
+        for aluno in alunos:
+            soma += aluno['Nota B1']
+            contador += 1
+        media = soma / contador if contador else 0
+        print(f"Média B1 da turma: {media:.2f}")
+    input("presione qualquer tecla para continuar!")
+    
+def media_b2(alunos):
+    if alunos:
+        soma = 0
+        contador = 0
+        for aluno in alunos:
+            soma += aluno['Nota B2']
+            contador += 1
+        media = soma / contador if contador else 0
+        print(f"Média B2 da turma: {media:.2f}")
+    input("presione qualquer tecla para continuar!")
+    
+def media_geral(alunos):
+    if alunos:
+        soma_b1 = 0
+        soma_b2 = 0
+        contador = 0
+        for aluno in alunos:
+            soma_b1 += aluno['Nota B1']
+            soma_b2 += aluno['Nota B2']
+            contador += 1
+        media_b1 = soma_b1 / contador if contador else 0
+        media_b2 = soma_b2 / contador if contador else 0
+        media_geral = (media_b1 + media_b2) / 2 if contador else 0
+        print(f"Média geral da turma: {media_geral:.2f}")
+    input("presione qualquer tecla para continuar!")
+    
+def diario():
+    cabecalho = '--------------------------------------------------------\n'
+    titulo = '                   Diario da turma\n'
+    colunas = 'RA    Nome                      Nota B1  Nota B2   Média\n'
+    
+    print(cabecalho + titulo + cabecalho + colunas + cabecalho)
+    
+    for aluno in alunos:
+        ra = aluno["RA"].ljust(5)
+        nome = aluno["Nome"].ljust(27)
+        nota_b1 = str(aluno["Nota B1"]).rjust(5)
+        nota_b2 = str(aluno["Nota B2"]).rjust(8)
+        media = str(aluno["Média"]).rjust(7)
+        print(f"{ra} {nome} {nota_b1} {nota_b2} {media}")
+        
+    print(cabecalho)
+    input("Pressione qualquer tecla para continuar!")
+
+def sair():
+    print("Saindo do programa...")
+    exit()
+
+if __name__ == "__main__":
+    while True:
+        match menu():
+            case 1:
+                add_aluno()
+            case 2:
+                listar_aluno()
+                break
+            case 3:
+                remover_aluno()
+                break
+            case 4:
+                procurar_aluno()
+                break
+            case 5:
+                aprovados()
+                break
+            case 6:
+                reprovados()
+                break
+            case 7:
+                procurar_nome()
+            case 8:
+                media_b1()
+            case 9:
+                media_b2()
+            case 10:
+                media_geral()
+            case 11:
+                diario()
+            case 0:
+                sair()
